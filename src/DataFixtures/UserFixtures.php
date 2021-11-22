098016f38fbcaeb336ff6f5703a611aeafb1356a
HenriquesSylvio <Henriques.Sylvio@outlook.fr>
2021-11-22 19:53:51 +0100

    Add Fixture for User, and testing to get JWT Token (Success)

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+use Faker;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface  $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager) : void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for($nbUsers = 1; $nbUsers <= 30; $nbUsers++){
+            $user = new User();
+            $user->setEmail($faker->email);
+            if($nbUsers === 1)
+                $user->setRoles(['ROLE_ADMIN']);
+            else
+                $user->setRoles(['ROLE_USER']);
+            $user->setPassword($this->passwordHasher->hashPassword($user, '54875487'));
+            $user->setLastName($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -38,9 +38,26 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+        
+        return $this;
     }
 
     public function getEmail(): ?string
@@ -88,6 +105,20 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
+
+        return $this;
+    }
+    
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
 
         return $this;
     }
